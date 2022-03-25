beada5bb60e293f14a5e7b767830daf17551fbfa
mray1997 <m.ray2013@yahoo.com>
2022-03-25 01:23:08 -0700

    Final Changes

--- searchAndAdd.test.ts
+++ searchAndAdd.test.ts
@@ -7,7 +7,7 @@ test("does a search", async () => {
 await  etsy.navigate();
 await etsy.driver.manage().window().maximize();
 await etsy.search("qa tester");
-expect(await etsy.getResults()).toContain("594 results,");
+expect(await etsy.getResults()).toContain("247 results,");
 await etsy.click(etsy.qaMug);
 let etsyTabs = await etsy.driver.getAllWindowHandles();
 await etsy.driver.switchTo().window(etsyTabs[1]);
